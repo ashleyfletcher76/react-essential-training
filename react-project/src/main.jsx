3096f54157dd512e5d859604fc294484eda7c980
Ashley Fletcher <ashley.fletcher76@yahoo.com>
2025-12-11 13:59:16 +0100

    branch finished

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const animals = ["giraffe", "zebra", "bear"]
+console.log(animals[1])
+
 createRoot(document.getElementById("root")).render(<App />);
