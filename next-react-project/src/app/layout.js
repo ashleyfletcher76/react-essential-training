b8d226837d66c592fb0e00ee30bdca5fe00bdb84
Ashley Fletcher <ashley.fletcher76@yahoo.com>
2025-12-11 17:06:59 +0100

    branch completed

--- next-react-project/src/app/layout.js
+++ next-react-project/src/app/layout.js
@@ -23,6 +23,9 @@ function Header() {
           <div>
             <Link href="/hotels">Hotels</Link>
           </div>
+          <div>
+            <Link href="/contact">Contact Page</Link>
+          </div>
         </nav>
       </div>
     </header>
